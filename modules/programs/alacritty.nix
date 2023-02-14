9ffd0a7ac868a2fb6ce80664a8a1cb1152b9e140
Lin Xianyi <iynaix@gmail.com>
2023-02-14 22:12:30 +0800

    Refactor alacritty catppuccin theme to use nix

--- modules/programs/alacritty.nix
+++ modules/programs/alacritty.nix
@@ -1,11 +1,4 @@
-{ pkgs, ... }: {
-  home = {
-    file.".config/alacritty" = {
-      source = ./alacritty;
-      recursive = true;
-    };
-  };
-
+{ pkgs, theme, ... }: {
   programs = {
     alacritty = {
       enable = true;
@@ -26,7 +19,106 @@
         };
         selection.save_to_clipboard = true;
         # window.opacity = 0.5;
-        import = [ "~/.config/alacritty/catppuccin-mocha.yml" ];
+        # catppuccin theme
+        colors = {
+          primary = {
+            background = theme.base;
+            foreground = theme.text;
+            # Bright and dim foreground colors
+            dim_foreground = theme.text;
+            bright_foreground = theme.text;
+          };
+
+          # Cursor colors
+          cursor = {
+            text = theme.base;
+            cursor = theme.rosewater;
+          };
+          vi_mode_cursor = {
+            text = theme.base;
+            cursor = theme.lavender;
+          };
+
+          # Search colors
+          search = {
+            matches = {
+              foreground = theme.base;
+              background = theme.subtext0;
+            };
+            focused_match = {
+              foreground = theme.base;
+              background = theme.green;
+            };
+            footer_bar = {
+              foreground = theme.base;
+              background = theme.subtext0;
+            };
+          };
+
+          # Keyboard regex hints
+          hints = {
+            start = {
+              foreground = theme.base;
+              background = theme.yellow;
+            };
+            end = {
+              foreground = theme.base;
+              background = theme.subtext0;
+            };
+          };
+
+          # Selection colors
+          selection = {
+            text = theme.base;
+            background = theme.rosewater;
+          };
+
+          # Normal colors
+          normal = {
+            black = theme.surface1;
+            red = theme.red;
+            green = theme.green;
+            yellow = theme.yellow;
+            blue = theme.blue;
+            magenta = theme.pink;
+            cyan = theme.teal;
+            white = theme.subtext1;
+          };
+          # Bright colors
+          bright = {
+            black = theme.surface2;
+            red = theme.red;
+            green = theme.green;
+            yellow = theme.yellow;
+            blue = theme.blue;
+            magenta = theme.pink;
+            cyan = theme.teal;
+            white = theme.subtext0;
+          };
+
+          # Dim colors
+          dim = {
+            black = theme.surface1;
+            red = theme.red;
+            green = theme.green;
+            yellow = theme.yellow;
+            blue = theme.blue;
+            magenta = theme.pink;
+            cyan = theme.teal;
+            white = theme.subtext1;
+          };
+
+          indexed_colors = [
+            {
+              index = 16;
+              color = "#FAB387";
+            }
+            {
+              index = 17;
+              color = "#F5E0DC";
+            }
+          ];
+        };
       };
     };
   };
