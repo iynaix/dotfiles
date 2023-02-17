c394ece99909f2ad29763b1294e1969948f50055
Lin Xianyi <iynaix@gmail.com>
2023-02-17 11:36:51 +0800

    Fix spice-vdagent for window managers

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -11,5 +11,20 @@
   services.spice-vdagentd.enable = true;
   services.spice-webdavd.enable = true;
 
+  # fix for spice-vdagentd not starting in wms
+  systemd.user.services.spice-agent = {
+    enable = true;
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.spice-vdagent}/bin/spice-vdagent -x";
+    };
+    unitConfig = {
+      ConditionVirtualization = "vm";
+      Description = "Spice guest session agent";
+      After = [ "graphical-session-pre.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+  };
+
   environment.systemPackages = with pkgs; [ nixfmt ];
 }
--- modules/programs/nemo.nix
+++ modules/programs/nemo.nix
@@ -2,12 +2,12 @@
   home = { packages = with pkgs; [ cinnamon.nemo dconf gnome.dconf-editor ]; };
 
   dconf.settings = {
-    "org.nemo.preferences" = {
+    "org/nemo/preferences" = {
       default-folder-viewer = "list-view";
       show-hidden-files = true;
       start-with-dual-pane = true;
     };
-    "org.nemo.preferences.menu-config" = {
+    "org/nemo/preferences/menu-config" = {
       selection-menu-make-link = true;
       selection-menu-move-to = true;
     };
