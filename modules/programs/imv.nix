304e406e0ee19cf40576d9f2efcd646937b9e9b0
Lin Xianyi <iynaix@gmail.com>
2023-07-14 21:11:35 +0800

    Fix imv infinite keypress loop

--- modules/programs/imv.nix
+++ modules/programs/imv.nix
@@ -14,19 +14,25 @@
     home-manager.users.${user} = {
       home.packages = [pkgs.imv];
 
-      xdg.configFile."imv/config".text =
-        /*
-        ini
-        */
-        ''
-          [options]
-          overlay = true
+      xdg.configFile."imv/config".text = ''
+        [options]
+        overlay = true
 
-          [binds]
-          i = overlay
-          x = exec rm "$imv_current_file"
-          w = exec hypr-wallpaper "$imv_current_file" &
-        '';
+        [binds]
+        i = overlay
+        x = exec rm "$imv_current_file"
+        w = exec ${pkgs.python3}/bin/python3 ${./wallust/hypr-wallpaper.py} "$imv_current_file"
+      '';
     };
+
+    nixpkgs.overlays = [
+      (self: super: {
+        # patch imv to not repeat keypresses causing waybar to launch infinitely
+        # https://github.com/eXeC64/imv/issues/207#issuecomment-604076888
+        imv = super.imv.overrideAttrs (oldAttrs: {
+          patches = [./imv-disable-key-repeat-timer.patch];
+        });
+      })
+    ];
   };
 }
