069296b30c9813a80034d937628fc6b4f2395a63
Lin Xianyi <iynaix@gmail.com>
2024-04-09 09:51:37 +0800

    Add default gitignore to templates

--- templates/default.nix
+++ templates/default.nix
@@ -1,31 +1,20 @@
 let
-  welcomeText = ''
-    # `.devenv` and `direnv` should be added to `.gitignore`
-    ```sh
-      echo .devenv >> .gitignore
-      echo .direnv >> .gitignore
-    ```
-  '';
   javascript = {
-    inherit welcomeText;
     path = ./javascript;
     description = "Javascript / Typescript dev environment";
   };
 
   python = {
-    inherit welcomeText;
     path = ./python;
     description = "Python dev environment";
   };
 
   rust = {
-    inherit welcomeText;
     path = ./rust;
     description = "Rust dev environment";
   };
 
   rust-stable = {
-    inherit welcomeText;
     path = ./rust-stable;
     description = "Rust (latest stable from fenix) dev environment";
   };
