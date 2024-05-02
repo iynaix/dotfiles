c0be6fc85a8ab04299cd8700a239d27ee990e1dd
Lin Xianyi <iynaix@gmail.com>
2024-05-02 17:03:04 +0800

    Update nixpkgs but keep hyprland at 0.38.1

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -17,12 +17,32 @@ lib.mkIf config.custom-nixos.hyprland.enable {
   # set here as legacy linux won't be able to set these
   hm.wayland.windowManager.hyprland = {
     enable = true;
-    package =
-      assert (
-        lib.assertMsg (pkgs.hyprland.version == "0.38.1") "hyprland: updated, sync with hyprnstack?"
-      );
-      pkgs.hyprland;
+    # package =
+    #   assert (
+    #     lib.assertMsg (pkgs.hyprland.version == "0.39.1") "hyprland: updated, sync with hyprnstack?"
+    #   );
+    #   pkgs.hyprland;
   };
+
+  # lock hyprland to 0.38.1 until workspace switching is resolved
+  nixpkgs.overlays = [
+    (_: prev: {
+      hyprland =
+        assert (
+          lib.assertMsg (prev.hyprland.version == "0.39.1") "hyprland: updated, sync with hyprnstack?"
+        );
+        prev.hyprland.overrideAttrs (_: rec {
+          version = "0.38.1";
+
+          src = prev.fetchFromGitHub {
+            owner = "hyprwm";
+            repo = "hyprland";
+            rev = "v${version}";
+            hash = "sha256-6y422rx8ScSkjR1dNYGYUxBmFewRYlCz9XZZ+XrVZng=";
+          };
+        });
+    })
+  ];
 
   xdg.portal = {
     enable = true;
