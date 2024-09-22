c5fe120be0ee4f3ed51ba0b904ddd76463b693fa
Elias Ainsworth <pilum-murialis@proton.me>
2024-09-22 13:25:33 -0700

    Update flake

--- home-manager/hyprland/startup.nix
+++ home-manager/hyprland/startup.nix
@@ -1,10 +1,14 @@
 {
   config,
+  host,
   lib,
   pkgs,
   user,
   ...
 }:
+let
+  openOnWorkspace = workspace: program: "[workspace ${toString workspace} silent] ${program}";
+in
 lib.mkIf config.custom.hyprland.enable {
   # start hyprland
   programs.bash.profileExtra = ''
@@ -30,39 +34,39 @@ lib.mkIf config.custom.hyprland.enable {
       "hyprctl setcursor ${config.home.pointerCursor.name} ${toString config.home.pointerCursor.size}"
 
       # browsers
-      # (openOnWorkspace 1 "brave --incognito")
-      # (openOnWorkspace 1 "brave --profile-directory=Default")
-      #
-      # # file manager
-      # (openOnWorkspace 4 "nemo")
-      #
-      # # terminal
-      # (openOnWorkspace 7 "$term")
-      #
-      # # firefox
-      # (openOnWorkspace 9 (
-      #   toString (
-      #     [
-      #       (lib.getExe config.programs.firefox.package)
-      #       "-P"
-      #       user # load user firefox profile
-      #       "https://discordapp.com/channels/@me"
-      #     ]
-      #     ++ lib.optionals (host == "desktop") [
-      #       "https://web.whatsapp.com" # requires access via local network
-      #       "http://localhost:9091" # transmission
-      #     ]
-      #   )
-      # ))
-      #
-      # # download desktop
-      # (openOnWorkspace 10 "$term nvim ${config.xdg.userDirs.desktop}/yt.txt")
-      # (openOnWorkspace 10 "$term")
-      #
-      # # focus the initial workspaces on startup
-      # "hyprctl dispatch workspace 9"
-      # "hyprctl dispatch workspace 7"
-      # "hyprctl dispatch workspace 1"
+      (openOnWorkspace 1 "brave --incognito")
+      (openOnWorkspace 1 "brave --profile-directory=Default")
+
+      # file manager
+      (openOnWorkspace 4 "nemo")
+
+      # terminal
+      (openOnWorkspace 7 "$term")
+
+      # firefox
+      (openOnWorkspace 9 (
+        toString (
+          [
+            (lib.getExe config.programs.firefox.package)
+            "-P"
+            user # load user firefox profile
+            "https://discordapp.com/channels/@me"
+          ]
+          ++ lib.optionals (host == "desktop") [
+            "https://web.whatsapp.com" # requires access via local network
+            "http://localhost:9091" # transmission
+          ]
+        )
+      ))
+
+      # download desktop
+      (openOnWorkspace 10 "$term nvim ${config.xdg.userDirs.desktop}/yt.txt")
+      (openOnWorkspace 10 "$term")
+
+      # focus the initial workspaces on startup
+      "hyprctl dispatch workspace 9"
+      "hyprctl dispatch workspace 7"
+      "hyprctl dispatch workspace 1"
     ];
   };
 }
