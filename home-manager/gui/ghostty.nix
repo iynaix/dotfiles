05a5d4f13435d6c66769244b7f9ee008c974ced1
Lin Xianyi <iynaix@gmail.com>
2025-01-04 21:20:13 +0800

    Update ghostty to use nixpkgs

--- home-manager/gui/ghostty.nix
+++ home-manager/gui/ghostty.nix
@@ -1,13 +1,11 @@
 {
   config,
-  inputs,
   lib,
   pkgs,
   ...
 }:
 let
   cfg = config.custom.ghostty;
-  ghostty = inputs.ghostty.packages.${pkgs.system}.default;
   inherit (config.custom) terminal;
 in
 {
@@ -23,12 +21,7 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home = {
-      packages = [ ghostty ];
-      sessionVariables = {
-        GHOSTTY_RESOURCES_DIR = "${ghostty}/share";
-      };
-    };
+    home.packages = [ pkgs.ghostty ];
 
     xdg.configFile."ghostty/config".text = lib.generators.toKeyValue {
       mkKeyValue = lib.generators.mkKeyValueDefault { } " = ";
@@ -54,6 +47,6 @@ in
       window-padding-y = terminal.padding;
     };
 
-    wayland.windowManager.hyprland.settings.bind = [ "$mod, q, exec, ${lib.getExe ghostty}" ];
+    wayland.windowManager.hyprland.settings.bind = [ "$mod, q, exec, ${lib.getExe pkgs.ghostty}" ];
   };
 }
