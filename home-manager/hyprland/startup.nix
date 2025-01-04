f8facbe8921b3febda14c7db39b23283ca9e11fa
Elias Ainsworth <pilum-murialis.toge@proton.me>
2025-01-03 23:18:50 -0700

    Just launch ghostty on startup.

--- home-manager/hyprland/startup.nix
+++ home-manager/hyprland/startup.nix
@@ -38,20 +38,23 @@ lib.mkIf config.custom.hyprland.enable {
 
       # browsers
       "hyprctl dispatch workspace 1"
-      (openOnWorkspace 1 "brave --profile-directory=Default")
-
-      # file manager
-      (openOnWorkspace 4 "nemo")
+      # (openOnWorkspace 1 "brave --profile-directory=Default")
+      #
+      # # file manager
+      # (openOnWorkspace 4 "nemo")
+      #
+      # # terminal
+      # (openOnWorkspace 7 "$term")
+      #
+      # # firefox
+      # (openOnWorkspace 9 (lib.getExe config.programs.firefox.package))
+      #
+      # # download desktop
+      # (openOnWorkspace 10 "$term nvim ${config.xdg.userDirs.desktop}/yt.txt")
+      # (openOnWorkspace 10 "$term")
 
       # terminal
-      (openOnWorkspace 7 "$term")
-
-      # firefox
-      (openOnWorkspace 9 (lib.getExe config.programs.firefox.package))
-
-      # download desktop
-      (openOnWorkspace 10 "$term nvim ${config.xdg.userDirs.desktop}/yt.txt")
-      (openOnWorkspace 10 "$term")
+      (openOnWorkspace 1 "ghostty")
     ];
   };
 
