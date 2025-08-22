bedf38f70ea3b9c24e3c11f83c8f742d8d1bf663
Lin Xianyi <iynaix@gmail.com>
2025-08-22 11:41:33 +0800

    Migrate ytdl to rust

--- home-manager/shell/yt-dlp.nix
+++ home-manager/shell/yt-dlp.nix
@@ -1,48 +1,7 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 let
   mkFormat =
     height: ''"bestvideo[height<=?${toString height}][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"'';
-
-  # use positional arguments if provided, otherwise use yt.txt
-  mkYtDlpWrapper = args: {
-    runtimeInputs = with pkgs; [
-      gawk
-      config.programs.yt-dlp.package
-    ];
-    text = # sh
-      let
-        ARCHIVE_FILE = "/tmp/ytdl-archive.txt";
-      in
-      ''
-        is_flag() {
-            [[ "$1" == -* ]]
-        }
-
-        args=(${args})
-
-        has_positional=false
-        for arg in "$@"; do
-            if ! is_flag "$arg"; then
-                has_positional=true
-            fi
-            args+=("$arg")
-        done
-
-        if ! $has_positional; then
-            # filter out non urls
-            while IFS= read -r url; do
-                if [[ $url == http* ]]; then
-                    args+=("$url")
-                fi
-            done < <(awk '!x[$0]++' "${config.xdg.userDirs.desktop}/yt.txt")
-        fi
-
-        pushd "${config.xdg.userDirs.download}" > /dev/null
-        echo > "${ARCHIVE_FILE}"
-        yt-dlp --download-archive "${ARCHIVE_FILE}"  "''${args[@]}"
-        popd > /dev/null
-      '';
-  };
 in
 {
   programs = {
@@ -63,15 +22,11 @@ in
   home = {
     shellAliases = {
       yt = "yt-dlp";
+      yt1080 = "ytdl --format ${mkFormat 1080}";
+      ytaudio = "ytdl --audio-format mp3 --extract-audio";
+      ytsub = "ytdl --write-auto-sub --sub-lang='en,eng' --convert-subs srt";
+      ytsubonly = "ytdl --write-auto-sub --sub-lang='en,eng' --convert-subs srt --skip-download --write-subs";
+      ytplaylist = "ytdl --output '%(playlist_index)d - %(title)s.%(ext)s'";
     };
-  };
-
-  custom.shell.packages = {
-    yt1080 = mkYtDlpWrapper "--format ${mkFormat 1080}";
-    ytdl = mkYtDlpWrapper "--no-cache";
-    ytaudio = mkYtDlpWrapper "--audio-format mp3 --extract-audio";
-    ytsub = mkYtDlpWrapper "--write-auto-sub --sub-lang='en,eng' --convert-subs srt";
-    ytsubonly = mkYtDlpWrapper "--write-auto-sub --sub-lang='en,eng' --convert-subs srt --skip-download --write-subs";
-    ytplaylist = mkYtDlpWrapper "--output '%(playlist_index)d - %(title)s.%(ext)s'";
   };
 }
---
+++ packages/dotfiles-rs/dotfiles/src/bin/ytdl.rs
@@ -0,0 +1,88 @@
+use std::{
+    collections::HashSet,
+    env,
+    fs::File,
+    io::{BufRead, BufReader, Write},
+    path::PathBuf,
+    process::Command,
+};
+
+use common::full_path;
+
+fn downloaded_files(archive_path: &PathBuf) -> Vec<String> {
+    if !archive_path.exists() {
+        return Vec::new();
+    }
+
+    let file = File::open(archive_path).expect("unable to open ytdl archive");
+    let reader = BufReader::new(file);
+
+    reader
+        .lines()
+        .map_while(Result::ok)
+        .filter_map(|line| line.strip_prefix("youtube ").map(str::to_string))
+        .collect()
+}
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().skip(1).collect(); // skip program name
+    // has positional arguments, urls / file is provided
+    let use_yt_txt = args.iter().all(|arg| arg.starts_with('-'));
+
+    let mut urls = HashSet::new();
+    let mut lines = Vec::new();
+
+    if use_yt_txt {
+        let yt_txt = full_path("~/Desktop/yt.txt");
+
+        let file = File::open(&yt_txt)?;
+        let reader = BufReader::new(file);
+
+        for line in reader.lines().map_while(Result::ok) {
+            lines.push(line.clone());
+            if line.starts_with("http") {
+                urls.insert(line);
+            }
+        }
+    }
+
+    // construct yt-dlp command
+    let mut cmd = Command::new("yt-dlp");
+
+    let archive_path = full_path("/tmp/ytdl-archive.txt");
+    cmd.arg("--download-archive").arg(&archive_path);
+    for arg in &args {
+        cmd.arg(arg);
+    }
+
+    // filter out already downloaded files
+    let already_downloaded = downloaded_files(&archive_path);
+    urls.retain(|url| !already_downloaded.iter().any(|yt_id| url.contains(yt_id)));
+    for url in &urls {
+        cmd.arg(url);
+    }
+
+    // cd to Downloads and run yt-dlp
+    let original_dir = env::current_dir()?;
+    env::set_current_dir(full_path("~/Downloads"))?;
+    let status = cmd.status()?;
+    env::set_current_dir(&original_dir)?;
+
+    // write yt.txt, removing already downloaded files
+    if use_yt_txt {
+        lines.retain(|line| !already_downloaded.iter().any(|yt_id| line.contains(yt_id)));
+
+        // trim trailing lines
+        while lines.last().is_some_and(|s| s.trim().is_empty()) {
+            lines.pop();
+        }
+
+        // write to yt.txt
+        let mut file = File::create(full_path("~/Desktop/yt.txt"))?;
+        for line in lines {
+            writeln!(file, "{line}")?;
+        }
+    }
+
+    std::process::exit(status.code().unwrap_or(1))
+}
