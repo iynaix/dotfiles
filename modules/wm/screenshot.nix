8389cd4d5683c55d91fa71ec06313cb0c82904e6
Lin Xianyi <iynaix@gmail.com>
2025-09-26 23:16:24 +0800

    Migrate screenshot.nix

---
+++ modules/wm/screenshot.nix
@@ -0,0 +1,115 @@
+{
+  config,
+  inputs,
+  isNixOS,
+  lib,
+  libCustom,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib)
+    mkAfter
+    mkIf
+    optionals
+    ;
+  focal = inputs.focal.packages.${pkgs.system}.default.override {
+    backend = config.custom.wm;
+    rofi = config.hm.programs.rofi.package;
+    ocr = true;
+  };
+in
+mkIf config.custom.isWm {
+  environment.systemPackages =
+    (with pkgs; [
+      swappy
+      wf-recorder
+    ])
+    ++ optionals isNixOS [ focal ];
+
+  # swappy conf
+  hj.files.".config/swappy/config".text = lib.generators.toINI { } {
+    default = {
+      save_dir = libCustom.homePath "Pictures/Screenshots";
+      save_filename_format = "%Y-%m-%dT%H:%M:%S%z.png";
+      show_panel = false;
+      line_size = 5;
+      text_size = 20;
+      text_font = "sans-serif";
+      paint_mode = "brush";
+      early_exit = false;
+      fill_shape = false;
+    };
+  };
+
+  custom.programs.hyprland.settings = {
+    bind = [
+      "$mod, backslash, exec, focal image --area selection --no-notify --no-save --no-rounded-windows"
+      "$mod_SHIFT, backslash, exec, focal image --edit swappy --rofi --no-rounded-windows"
+      "$mod_CTRL, backslash, exec, focal image --area selection --ocr"
+      "ALT, backslash, exec, focal video --rofi --no-rounded-windows"
+    ];
+  };
+
+  # add focal module to waybar
+  hm = {
+    custom.waybar = {
+      config = {
+        "custom/focal" = {
+          exec = # sh
+            ''focal-waybar --recording "󰑋"'';
+          format = "{}";
+          # hide-empty-text = true;
+          # return-type = "json";
+          on-click = "focal video --stop";
+          interval = 2; # poll every 2s
+        };
+
+        modules-left = mkAfter [ "custom/focal" ];
+      };
+
+      extraCss = # css
+        ''
+          #custom-focal {
+            font-size: 24px;
+          }
+        '';
+    };
+
+    programs.niri.settings = {
+      binds = {
+        "Mod+backslash".action.screenshot = {
+          show-pointer = false;
+        };
+        "Mod+Shift+backslash".action.spawn = [
+          "focal"
+          "image"
+          # "--edit"
+          # "swappy"
+          "--rofi"
+        ];
+        # "Mod+Ctrl+backslash".action.spawn = [
+        #   "focal"
+        #   "image"
+        #   "--area"
+        #   "selection"
+        #   "--ocr"
+        # ];
+        "Alt+backslash".action.spawn = [
+          "focal"
+          "video"
+          "--rofi"
+        ];
+      };
+    };
+
+    custom.mango.settings = {
+      bind = [
+        "$mod, backslash, spawn, focal image --area selection --no-notify --no-save --no-rounded-windows"
+        "$mod+SHIFT, backslash, spawn, focal image --edit swappy --rofi --no-rounded-windows"
+        "$mod+CTRL, backslash, spawn, focal image --area selection --ocr"
+        "ALT, backslash, spawn, focal video --rofi --no-rounded-windows"
+      ];
+    };
+  };
+}
